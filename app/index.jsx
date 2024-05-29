00248c5038ca78bfc0258b201becfd296a5e4f4c
lawliet005 <sabalomsadze912@gmail.com>
2024-05-29 17:20:39 +0400

    base layer

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex flex-row bg-slate-500 text-gray-950 w-4" >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
